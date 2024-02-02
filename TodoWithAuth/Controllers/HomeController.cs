f92f29bca73536ba573746f2e5d7d412916e0590
mylongcode <mylongcode@gmail.com>
2024-02-03 00:42:46 +0500

    initial commit

--- TodoWithAuth/Controllers/HomeController.cs
+++ TodoWithAuth/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TodoWithAuth.Models;
 
@@ -17,7 +18,7 @@ namespace TodoWithAuth.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
--- TodoWithAuth/Data/ApplicationDbContext.cs
+++ TodoWithAuth/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace TodoWithAuth.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
     }
 }
